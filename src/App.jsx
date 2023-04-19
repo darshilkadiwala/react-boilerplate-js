fcad5e030034f21d5a3e422fe100ae9fdbe7defa
darshilkadiwala <darshilkadiwala2020@gmail.com>
2023-04-19 08:36:21 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      The React boilerplate code with Redux-toolkit, Redux-saga, and AntD desgin
+    </div>
+  );
+}
+
+export default App;
