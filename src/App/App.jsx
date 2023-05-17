58169b1f0d99d1d8e1be82de277ef810df0f0a01
darshilkadiwala <darshilkadiwala2020@gmail.com>
2023-05-17 09:20:48 +0530

    used root router in app

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,11 @@
+import RootRoutes from "../common/routers/root.routes.jsx";
 import "./App.css";
 
 function App() {
   return (
     <div>
       The React boilerplate code with Redux-toolkit, Redux-saga, and AntD desgin
+      <RootRoutes />
     </div>
   );
 }
