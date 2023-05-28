eadf75dfed2bec54ceafd3cd868c6904830ef7a2
darshilkadiwala <darshilkadiwala2020@gmail.com>
2023-05-28 18:46:17 +0530

    code clean up: remove Ant Design library

---
+++ src/common/providers/AppProvider.jsx
@@ -0,0 +1,7 @@
+import RootRoutes from "../routers/root.routes";
+
+function AppProvider() {
+  return <RootRoutes />;
+}
+
+export default AppProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App/App";
+import AppProvider from "./common/providers/AppProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AppProvider />
   </React.StrictMode>
 );
